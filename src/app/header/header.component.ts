621d0a65d7a6926f101d6ef9105540e7f1ba316a
Yoga Setiadi <yogarevysetiadi@gmail.com>
2020-03-03 12:17:16 +0700

    Create current url and fix typo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,24 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
+  aktip = 0;
   constructor() { }
+  
+  ngOnInit() {
+    let allUrl = window.location.href;
+    let currentUrl = allUrl.replace('http://localhost:8100/','/')
 
-  ngOnInit() {}
+    if (currentUrl==='/home') {
+      this.aktip = 1;
+    } else if(currentUrl==='/about') {
+      this.aktip = 2;
+    } else if(currentUrl==='/work_exp') {
+      this.aktip = 3;
+    } else if(currentUrl==='/portofolio') {
+      this.aktip = 4;
+    } else if(currentUrl==='/skills') {
+      this.aktip = 5;
+    } else {
+      this.aktip = 6;
+    }
+  }
 
   menus = [
     {"id" : "1", "menu": "HI!", "route" : "/home"},
     {"id" : "2", "menu": "ABOUT ME", "route" : "/about"},
     {"id" : "3", "menu": "WORK EXPERIENCE", "route" : "/work_exp"},
-    {"id" : "4", "menu": "PORTOFOLIO", "route" : "/portofolia"},
+    {"id" : "4", "menu": "PORTOFOLIO", "route" : "/portofolio"},
     {"id" : "5", "menu": "SKILLS", "route" : "/skills"},
     {"id" : "6", "menu": "CONTACT", "route" : "/contact"}
   ];
 
-  aktip = 1;
-
-  clickMenu(e) {
-    this.aktip = e.id;
-    console.log(this.aktip);
+  clickMenu($event) {
+    this.aktip = $event.id;
   }
 }
